497d01aee1ea209120ad87273e66c4cb2c6155a7
richard <liuchang85501@gmail.com>
2023-05-09 11:58:16 +0800

    Add get data

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ColorCards from './components/ColorCards';
 import MainInfo from './components/MainInfo';
 import Percent from './components/Percent';
@@ -6,6 +6,16 @@ import Activities from './components/Activities';
 import Detail from './components/Detail';
 
 const Dashboard = () => {
+  useEffect(() => {
+    fetch(
+      'https://sheets.googleapis.com/v4/spreadsheets/1lw17u6YExrljLci0vrPVx5WuToUB6W1mCnnLQVdYdEg/values/trade_record_database?alt=json&key=AIzaSyCTVKBmNnCVjgos3DRJvH2CWvbzplUQ73g'
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
+
   return (
     <body className='p-8 bg-gray-100'>
       <div className='flex'>
